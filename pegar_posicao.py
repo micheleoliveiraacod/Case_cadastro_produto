00229ef5962f64b36ce4fbb617fecc34b7560137
Michele Oliveira <data.analystmlso@gmail.com>
2026-01-17 09:19:48 -0300

    Create pegar_posicao.py

---
+++ pegar_posicao.py
@@ -0,0 +1,8 @@
+import time
+import pyautogui
+
+#executar este codigo e enquanto ele espera, passar o mouse em cima do local que quer pegar a posiçao
+time.sleep(5)
+print(pyautogui.position()) #pegar a posição que aparecer aqui e colocar lá no codigo
+
+pyautogui.scroll(200)
